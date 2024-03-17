160f5590427af56c1f3baffd2f50a9dbc5e4b76c
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-03-17 13:27:36 +0330

    [fix]-serverideProp

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,24 @@
 import Footer from '@/components/Footer';
-import PostCardSkeleton from '@/components/PostCardSkeleton';
 import PostList from '@/components/PostList';
 import { db } from '@/lib/db';
-import { Suspense } from 'react';
-import Image from 'next/image';
-// import { blog4 } from '@/public';
-import blog4 from '@/public/blog4.svg';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-async function getPosts() {
-  const posts = await db.post.findMany({
-    select: {
-      id: true,
-      name: true,
-      content: true,
-      tag: true,
-    },
-    orderBy: {
-      createAt: 'desc',
-    },
-  });
-  return posts;
-}
 
-export default async function Home() {
-  const posts = await getPosts();
+export const getServerSideProps = async () => {
+  const posts = await db.post.findMany({
+    select: { id: true, name: true, content: true, tag: true },
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function Home({ posts }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <PostList posts={posts} />
-
       <Footer />
     </div>
   );
