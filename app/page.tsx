e79ed8b9cfb0495ad7e11f1c443109a3539dcf38
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-03-17 13:36:22 +0330

    '[fix]-getServerideProps

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
+// app/page.tsx
 import Footer from '@/components/Footer';
 import PostList from '@/components/PostList';
 import { db } from '@/lib/db';
 
-export const getServerSideProps = async () => {
+export default async function Home() {
   const posts = await db.post.findMany({
     select: { id: true, name: true, content: true, tag: true },
     orderBy: { createdAt: 'desc' },
   });
 
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-export default function Home({ posts }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <PostList posts={posts} />
