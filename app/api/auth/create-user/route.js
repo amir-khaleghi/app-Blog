b74a188b9fe29a8f5699fecc5a12ad5f1a9dc8d9
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-05-06 11:33:22 +0330

    [Deploy]: deployment on vercel

--- app/api/auth/create-user/route.js
+++ app/api/auth/create-user/route.js
@@ -39,7 +39,7 @@ export async function GET() {
       }
     }
 
-    return NextResponse.redirect('https://blog-app.liara.run/home');
+    return NextResponse.redirect('https://app-blog-beryl.vercel.app/home');
   } catch (err) {
     console.error('Internal Server Error:', err);
     return NextResponse.json(
